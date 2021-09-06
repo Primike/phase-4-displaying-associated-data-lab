ac680fd835bcf3f9a58602e8386c74e7420642ab
Prince <avecillasprince@yahoo.com>
2021-09-05 23:01:40 -0400

    commit tree 8eea01947d08606c22d4dd7318b2c9aaa13340c1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+    def show 
+        user = User.find(params[:id])
+        if user
+            render json: user, include: :items
+        else
+            render json: { error: "Bird not found" }, status: :not_found
+        end
+    end
 end
